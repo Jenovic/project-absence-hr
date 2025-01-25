73320e5c4f5cf641e7241e7d1a7d7004a4c75138
JENOVICBOOK\lumuj <lumujenovic@gmail.com>
2025-01-25 18:10:49 +0000

    WIP: Add an index for my util exports

---
+++ my-app/src/utils/index.ts
@@ -0,0 +1,4 @@
+import { absenceListheaders } from "./constants";
+import { fetchCall, calculateEndDate, sortAbsences} from "./helper";
+
+export { absenceListheaders, calculateEndDate, sortAbsences, fetchCall };
